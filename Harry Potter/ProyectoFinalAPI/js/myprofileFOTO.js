5c0d663b984fcb98c45e0341ba2949b4ae05cb5a
Dibenhi Garza <dibenhi120@gmail.com>
2025-12-09 16:11:42 -0700

    Delete Harry Potter directory

--- Harry Potter/ProyectoFinalAPI/js/myprofileFOTO.js
+++
@@ -1,9 +0,0 @@
-//IMAGEN USUARIO 
-/*const changePicBtn = document.querySelector(".cambiaFoto");
-
-if (changePicBtn) {
-        changePicBtn.addEventListener("click", () => {
-            alert("Profile pic wants to be changed! (Nothing actually happened because this is done afterwards dearest)");
-    });
-}
-*/
